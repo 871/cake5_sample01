d44f2cba74b28f420d09d1467343be43dce41bbf
花井　洋幸 <hiro871@hanaihiroyukinoMacBook-Air.local>
2026-03-01 20:49:40 +0900

    コントローラサービス用プロセスクラス群作成

--- src/Lib/UUID/UUID.php
+++ src/Lib/UUID/UUID.php
@@ -346,8 +346,8 @@ class UUID
         } elseif ($version === 8) {
             $unixts = hexdec(substr($timehex, 0, 13));
             $subsec = self::decodeSubsec(
-                (int)(hexdec(substr($timehex, 13)) << 2) 
-                + (hexdec(substr($uuid, 16, 1)) & 0x03)
+                (int)(hexdec(substr($timehex, 13)) << 2)
+                + (hexdec(substr($uuid, 16, 1)) & 0x03),
             );
             $retval = strval($unixts * self::V8_SUBSEC_RANGE + $subsec);
             $retval = substr_replace(str_pad($retval, 8, '0', STR_PAD_LEFT), '.', -7, 0);
