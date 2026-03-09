973fbec228ea4160973a37d965bac1dd8ddbd966
花井　洋幸 <hiro871@hanaihiroyukinoMacBook-Air.local>
2026-03-09 13:42:06 +0900

    途中コミット

--- src/Infrastructure/Persistence/Cake/Sample/MySqlTypeSamplesRepository.php
+++ src/Infrastructure/Persistence/Cake/Sample/MySqlTypeSamplesRepository.php
@@ -6,83 +6,27 @@ namespace App\Infrastructure\Persistence\Cake\Sample;
 use App\Domain\Sample\MySqlTypeSamples\Entity\MySqlTypeSample as DomainEntity;
 use App\Domain\Sample\MySqlTypeSamples\Repository\MySqlTypeSamplesRepository as DomainMySqlTypeSamplesRepository;
 use App\Domain\Sample\MySqlTypeSamples\SearchCondition;
-use App\Model\Table\Sample\MySqlTypeSamplesTable;
-use App\Service\Input\Normalizer\RecursiveEmptyStringToNullNormalizer;
-use Cake\Database\Expression\QueryExpression;
 use Cake\ORM\Query;
 
 class MySqlTypeSamplesRepository implements DomainMySqlTypeSamplesRepository
 {
-    /**
-     * @var \App\Model\Table\Sample\MySqlTypeSamplesTable
-     */
-    private MySqlTypeSamplesTable $mySqlTypeSamplesTable;
-
-    /**
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->mySqlTypeSamplesTable = MySqlTypeSamplesTable::getInstance();
-    }
-
     /**
      * @param \App\Domain\Sample\MySqlTypeSamples\SearchCondition $condition
      * @return \Cake\ORM\Query
      */
     public function search(SearchCondition $condition): Query
     {
-        return $this->mySqlTypeSamplesTable
-            ->find()
-            ->select([
-                'MySqlTypeSamples__id' => 'MySqlTypeSamples.id',
-                'MySqlTypeSamples__int_col' => 'MySqlTypeSamples.int_col',
-                'MySqlTypeSamples__bigint_col' => 'MySqlTypeSamples.bigint_col',
-                'MySqlTypeSamples__decimal_col' => 'MySqlTypeSamples.decimal_col',
-                'MySqlTypeSamples__float_col' => 'MySqlTypeSamples.float_col',
-                'MySqlTypeSamples__double_col' => 'MySqlTypeSamples.double_col',
-                'MySqlTypeSamples__date_col' => 'MySqlTypeSamples.date_col',
-                'MySqlTypeSamples__time_col' => 'MySqlTypeSamples.time_col',
-                'MySqlTypeSamples__datetime_col' => 'MySqlTypeSamples.datetime_col',
-                'MySqlTypeSamples__char_col' => 'MySqlTypeSamples.char_col',
-                'MySqlTypeSamples__varchar_col' => 'MySqlTypeSamples.varchar_col',
-                'MySqlTypeSamples__text_col' => 'MySqlTypeSamples.text_col',
-                'MySqlTypeSamples__mediumtext_col' => 'MySqlTypeSamples.mediumtext_col',
-                'MySqlTypeSamples__longtext_col' => 'MySqlTypeSamples.longtext_col',
-                'MySqlTypeSamples__json_col' => 'MySqlTypeSamples.json_col',
-            ])
-            ->where(
-                array_filter([
-                    'MySqlTypeSamples.id' => $condition->getId()->toString(),
-                    'MySqlTypeSamples.int_col >=' => $condition->getIntColFrom()->toString(),
-                    'MySqlTypeSamples.int_col <=' => $condition->getIntColTo()->toString(),
-                    'MySqlTypeSamples.bigint_col >=' => $condition->getBigintColFrom()->toString(),
-                    'MySqlTypeSamples.bigint_col <=' => $condition->getBigintColTo()->toString(),
-                    'MySqlTypeSamples.decimal_col >=' => $condition->getDecimalColFrom()->toString(),
-                    'MySqlTypeSamples.decimal_col <=' => $condition->getDecimalColTo()->toString(),
-                    'MySqlTypeSamples.float_col >=' => $condition->getFloatColFrom()->toString(),
-                    'MySqlTypeSamples.float_col <=' => $condition->getFloatColTo()->toString(),
-                    'MySqlTypeSamples.double_col >=' => $condition->getDoubleColFrom()->toString(),
-                    'MySqlTypeSamples.double_col <=' => $condition->getDoubleColTo()->toString(),
-                    'MySqlTypeSamples.date_col >=' => $condition->getDateColFrom()->toString(),
-                    'MySqlTypeSamples.date_col <=' => $condition->getDateColTo()->toString(),
-                    'MySqlTypeSamples.time_col >=' => $condition->getTimeColFrom()->toString(),
-                    'MySqlTypeSamples.time_col <=' => $condition->getTimeColTo()->toString(),
-                    'MySqlTypeSamples.datetime_col >=' => $condition->getDatetimeColFrom()->toString(),
-                    'MySqlTypeSamples.datetime_col <=' => $condition->getDatetimeColTo()->toString(),
-                    $condition->getKeyword()->toString() !== '' ? new QueryExpression(
-                        'MATCH(MySqlTypeSamples.search_text) AGAINST(:keyword IN BOOLEAN MODE)',
-                    ) : new QueryExpression(":keyword = ''"),
-                ], fn($v) => $v !== ''),
-            )
-            ->bind(':keyword', $condition->getKeyword()->toString(), 'string')
-            ->formatResults(function ($results) {
-                return $results->map(function ($entity) {
-                    /** @var array<string, ?string> $data */
-                    $data = (array)(new RecursiveEmptyStringToNullNormalizer())->normalize($entity->toArray());
+        return (new MySqlTypeSamplesRepository\Search($condition))->run();
+    }
 
-                    return new DomainEntity(...$data);
-                });
-            });
+    /**
+     * 新規作成
+     *
+     * @param \App\Domain\Sample\MySqlTypeSamples\Entity\MySqlTypeSample $domainEntity
+     * @return \App\Domain\Sample\MySqlTypeSamples\Entity\MySqlTypeSample
+     */
+    public function create(DomainEntity $domainEntity): DomainEntity
+    {
+        return (new MySqlTypeSamplesRepository\Create($domainEntity))->run();
     }
 }
---
+++ src/Infrastructure/Persistence/Cake/Sample/MySqlTypeSamplesRepository/Create.php
@@ -0,0 +1,105 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infrastructure\Persistence\Cake\Sample\MySqlTypeSamplesRepository;
+
+use App\Domain\Exception\RepositoryException;
+use App\Domain\Sample\MySqlTypeSamples\Entity\MySqlTypeSample as DomainEntity;
+use App\Model\Entity\Sample\MySqlTypeSample as OrmEntity;
+use App\Model\Table\Sample\MySqlTypeSamplesTable;
+use App\Security\Input\Cast;
+use Cake\ORM\Exception\PersistenceFailedException;
+use Cake\ORM\Locator\LocatorAwareTrait;
+
+final class Create
+{
+    use LocatorAwareTrait;
+
+    /**
+     * @var \App\Model\Table\Sample\MySqlTypeSamplesTable
+     */
+    private MySqlTypeSamplesTable $table;
+
+    /**
+     * @param \App\Domain\Sample\MySqlTypeSamples\Entity\MySqlTypeSample $domainEntity
+     */
+    public function __construct(
+        private readonly DomainEntity $domainEntity,
+    ) {
+        $this->table = $this->fetchTable(MySqlTypeSamplesTable::class);
+    }
+
+    /**
+     * @return \App\Domain\Sample\MySqlTypeSamples\Entity\MySqlTypeSample
+     */
+    public function run(): DomainEntity
+    {
+        try {
+            /** @var \App\Model\Entity\Sample\MySqlTypeSample $ormEntity */
+            $ormEntity = $this->table->getConnection()->transactional(
+                function (): OrmEntity {
+                    $entity = $this->createOrmEntity();
+
+                    return $this->table->saveOrFail($entity, [
+                        'checkExisting' => false,
+                    ]);
+                },
+            );
+        } catch (PersistenceFailedException $ex) {
+            throw new RepositoryException(
+                message: 'MySqlTypeSamplesRepository Create Error',
+                previous: $ex,
+            );
+        }
+
+        return $this->convertDomainEntity($ormEntity);
+    }
+
+    /**
+     * @return \App\Model\Entity\Sample\MySqlTypeSample
+     */
+    private function createOrmEntity(): OrmEntity
+    {
+        return $this->table->newEntity([
+            'int_col' => $this->domainEntity->intCol()->toString(),
+            'bigint_col' => $this->domainEntity->bigintCol()->toString(),
+            'decimal_col' => $this->domainEntity->decimalCol()->toString(),
+            'float_col' => $this->domainEntity->floatCol()->toString(),
+            'double_col' => $this->domainEntity->doubleCol()->toString(),
+            'date_col' => $this->domainEntity->dateCol()->toString(),
+            'time_col' => $this->domainEntity->timeCol()->toString(),
+            'datetime_col' => $this->domainEntity->datetimeCol()->toString(),
+            'char_col' => $this->domainEntity->charCol()->toString(),
+            'varchar_col' => $this->domainEntity->varcharCol()->toString(),
+            'text_col' => $this->domainEntity->textCol()->toString(),
+            'mediumtext_col' => $this->domainEntity->mediumtextCol()->toString(),
+            'longtext_col' => $this->domainEntity->longtextCol()->toString(),
+            'json_col' => $this->domainEntity->jsonCol()->toString(),
+        ]);
+    }
+
+    /**
+     * @param \App\Model\Entity\Sample\MySqlTypeSample $ormEntity
+     * @return \App\Domain\Sample\MySqlTypeSamples\Entity\MySqlTypeSample
+     */
+    private function convertDomainEntity(OrmEntity $ormEntity): DomainEntity
+    {
+        return new DomainEntity(
+            id: $ormEntity->id,
+            int_col: Cast::toString($ormEntity->int_col),
+            bigint_col: Cast::toString($ormEntity->bigint_col),
+            decimal_col: $ormEntity->decimal_col,
+            float_col: (string)$ormEntity->float_col,
+            double_col: (string)$ormEntity->double_col,
+            date_col: $ormEntity->date_col?->format('Y-m-d'),
+            time_col: $ormEntity->time_col?->format('H:i:s'),
+            datetime_col: $ormEntity->datetime_col?->format('Y-m-d H:i:s'),
+            char_col: $ormEntity->char_col,
+            varchar_col: $ormEntity->varchar_col,
+            text_col: $ormEntity->text_col,
+            mediumtext_col: $ormEntity->mediumtext_col,
+            longtext_col: $ormEntity->longtext_col,
+            json_col: json_encode($ormEntity->json_col) ?: null,
+        );
+    }
+}
